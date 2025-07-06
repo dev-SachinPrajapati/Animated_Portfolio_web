541054f14cdb0ffdc1183d7f55b4f65618af0346
Sachin Prajapati <captsachin8898@gmail.com>
2025-07-06 22:04:35 +0530

    Change in layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Sachin Prajapati | Full-Stack Developer & SDE Fresher",
   description: "Portfolio of Sachin Prajapati, showcasing projects, internships, and skills in full-stack development, system design, and problem-solving.",
-  icons: {
-    icon: "/profile.png", 
-  },
 };
 
 export default function RootLayout({
